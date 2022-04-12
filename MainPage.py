80945e812899b6db9c238629f9cd5b0555e0826d
Talida <75331740+Talida-M@users.noreply.github.com>
2022-04-12 22:06:40 +0300

    Main Page-schelet

---
+++ MainPage.py
@@ -0,0 +1,15 @@
+from kivy.app import  App
+from kivy.uix.widget import  Widget
+from kivy.core.window import Window
+
+class MainWidget(Widget):
+    pass
+    Window.clearcolor = (0.92, 0.76 , 0.63)
+
+
+class MainPageApp(App):
+    pass
+
+
+MainPageApp().run()
+#GridLayout
